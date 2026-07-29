e226e5db03fa641b0274b8f015962fc94a59fc29
devlucasaf <freitas.lucasaf@gmail.com>
2026-07-29 15:43:04 -0300

    adiciona código de pipeline

--- src/main/java/application/exercicios/basico/pipeline/Main.java
+++ src/main/java/application/exercicios/basico/pipeline/Main.java
@@ -1,4 +1,73 @@
 package application.exercicios.basico.pipeline;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import application.exercicios.basico.pipeline.entity.Funcionario;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o caminho completo do arquivo: ");
+
+        String path = scanner.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Funcionario> lista = new ArrayList<>();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] campos = line.split(",");
+
+                String nome = campos[0];
+                String email = campos[1];
+                Double salario = Double.parseDouble(campos[2]);
+
+                lista.add(new Funcionario(nome, email, salario));
+
+                line = br.readLine();
+            }
+
+            System.out.print("Digite o salário: ");
+            double salario = scanner.nextDouble();
+
+            List<String> emails = lista.stream()
+                    .filter(e -> e.getSalario() > salario)
+                    .map(e -> e.getEmail())
+                    .collect(Collectors.toList());
+
+            System.out.println("E-mails das pessoas cujo salário é maior que "
+                            + String.format("%.2f", salario)
+                            + ":"
+            );
+
+            emails.forEach(System.out::println);
+
+            double sum = lista.stream()
+                    .filter(e -> e.getNome().startsWith("M"))
+                    .map(e -> e.getSalario())
+                    .reduce(0.0, (x, y) -> x + y);
+
+            System.out.println(
+                    "Soma dos salários das pessoas cujo nome começa com 'M': "
+                            + String.format("%.2f", sum)
+            );
+
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
 }
--- src/main/java/application/exercicios/basico/pipeline/entity/Funcionario.java
+++ src/main/java/application/exercicios/basico/pipeline/entity/Funcionario.java
@@ -1,4 +1,37 @@
 package application.exercicios.basico.pipeline.entity;
 
 public class Funcionario {
+    private String nome;
+    private String email;
+    private Double salario;
+
+    public Funcionario(String nome, String email, Double salario) {
+        this.nome = nome;
+        this.email = email;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
 }
