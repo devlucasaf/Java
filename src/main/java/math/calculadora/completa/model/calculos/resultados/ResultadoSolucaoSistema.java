aaa213852a4ee566a5251bc8ec34272effe763b4
beneficios computações <freitas.lucasaf@gmail.com>
2026-08-11 00:38:47 -0300

    Renomeia o nomedo arquivo

--- src/main/java/math/calculadora/completa/model/calculos/resultados/ResultadoSolucaoSistema.java
+++ src/main/java/math/calculadora/completa/model/calculos/resultados/ResultadoSolucaoSistema.java
@@ -1,10 +1,10 @@
 package math.calculadora.completa.model.calculos.resultados;
 
-public class SolucaoSistema {
+public class ResultadoSolucaoSistema {
     public final double x;
     public final double y;
 
-    public SolucaoSistema(double x, double y) {
+    public ResultadoSolucaoSistema(double x, double y) {
         this.x = x;
         this.y = y;
     }
