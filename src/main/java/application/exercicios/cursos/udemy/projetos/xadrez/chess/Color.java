8f90240965d011dd510c4988c8a30af8b32f3bfe
devlucasaf <freitas.lucasaf@gmail.com>
2026-09-23 15:18:10 -0300

    moved

---
+++ src/main/java/application/exercicios/cursos/udemy/projetos/xadrez/chess/Color.java
@@ -0,0 +1,6 @@
+package application.exercicios.cursos.udemy.projetos.xadrez.chess;
+
+public enum Color {
+    BLACK,
+    WHITE;
+}
--- src/main/java/cursos/udemy/projects/xadrez/chess/Color.java
+++
@@ -1,6 +0,0 @@
-package cursos.udemy.projects.xadrez.chess;
-
-public enum Color {
-    BLACK,
-    WHITE;
-}
